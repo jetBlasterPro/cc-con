106f7f5e62b1c04b446dda16bd4c89ddf02b6005
jetBlasterPro <jetmeteor1@protonmail.com>
2025-07-03 23:23:11 +0530

    p4

---
+++ gitactions-mySQL/create_tables.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS demo;
+USE demo;
+
+CREATE TABLE students(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(100),
+    marks INT
+);
+
+INSERT INTO students (name, marks) VALUES
+("Pranav", 85)
+("Pratham", 90),
+("Omkar", 76);
